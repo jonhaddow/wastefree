119722e6a2a1afa9c5689c3f561add39b2080d68
Jon Haddow <haddowjon@protonmail.com>
2019-09-29 21:57:40 +0100

    Add search option to navigation bar

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
 import styles from "./navigation.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navigation(): JSX.Element {
 	const currentUrl = window.location.pathname;
@@ -36,9 +38,36 @@ export default function Navigation(): JSX.Element {
 		}
 	);
 
+	const searchBox = React.createRef<HTMLInputElement>();
+	const [searchOpen, setSearchOpen] = useState(false);
+	const onSearchOpen = (): void => {
+		setSearchOpen(!searchOpen);
+	};
+
+	useEffect((): void => {
+		if (searchBox.current) {
+			searchBox.current.focus();
+		}
+	});
+
 	return (
 		<nav className={styles.mainNav}>
-			<ul>{listItems}</ul>
+			{searchOpen ? (
+				<input
+					ref={searchBox}
+					className={`${styles.searchBar} ${
+						searchOpen ? styles.active : ""
+					}`}
+					placeholder="Search for content"
+				></input>
+			) : (
+				<ul>{listItems}</ul>
+			)}
+			<button onClick={onSearchOpen}>
+				<FontAwesomeIcon
+					icon={searchOpen ? faTimes : faSearch}
+				></FontAwesomeIcon>
+			</button>
 		</nav>
 	);
 }
