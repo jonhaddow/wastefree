7a338dbbf1d8fc4ae916e40b5168c075eb6bf5e9
Jon Haddow <haddowjon@protonmail.com>
2019-10-05 20:12:22 +0100

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../components/layout";
+import { Link } from "gatsby";
+
+export default function _404Page(): JSX.Element {
+	return (
+		<Layout>
+			<section>
+				<h2>404</h2>
+				<p>
+					The page could not be found. Go back to the{" "}
+					<Link to="/">Home page</Link>.
+				</p>
+			</section>
+		</Layout>
+	);
+}
