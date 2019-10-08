9ce733f61a1723a6720ebcd027e6e7f35ec984fa
Jon Haddow <haddowjon@protonmail.com>
2019-10-08 21:35:38 +0100

    Setup for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,22 @@ module.exports = {
 			options: {
 				siteUrl: "https://headless.wastefreemama.com"
 			}
-		}
+		},
+
+		// PWA
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Waste Free Mama`,
+				start_url: `/`,
+				background_color: `#6b37bf`,
+				theme_color: `#6b37bf`,
+				// Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+				// see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+				display: `standalone`,
+				icon: `src/assets/favicon.png` // This path is relative to the root of the site.
+			}
+		},
+		"gatsby-plugin-offline"
 	]
 };
