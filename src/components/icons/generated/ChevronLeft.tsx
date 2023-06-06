72b608292dbce2aba329d0ece863b1d13a739329
Jon Haddow <jon@haddow.me>
2023-06-06 19:25:24 +0100

    Update Icons

--- src/components/icons/generated/ChevronLeft.tsx
+++ src/components/icons/generated/ChevronLeft.tsx
@@ -1,11 +1,8 @@
 import * as React from "react";
-
-function SvgChevronLeft(props: React.SVGProps<SVGSVGElement>) {
-	return (
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
-			<path d="M18.355 21.073L9.282 12l9.073-9.073A1.713 1.713 0 1015.931.503L5.646 10.788c-.67.67-.67 1.755 0 2.424l10.285 10.285a1.713 1.713 0 102.424-2.424z" />
-		</svg>
-	);
-}
-
+import type { SVGProps } from "react";
+const SvgChevronLeft = (props: SVGProps<SVGSVGElement>) => (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" {...props}>
+		<path d="M18.355 21.073 9.282 12l9.073-9.073A1.713 1.713 0 1 0 15.931.503L5.646 10.788c-.67.67-.67 1.755 0 2.424l10.285 10.285a1.713 1.713 0 1 0 2.424-2.424z" />
+	</svg>
+);
 export default SvgChevronLeft;
